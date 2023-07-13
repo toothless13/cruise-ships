03b14cb88445d22fc83d7aae24253731b6fd664f
Tim <50870607+toothless13@users.noreply.github.com>
2023-07-13 21:17:13 +0100

    Ship constructor and tests

---
+++ __tests__/Ship.test.js
@@ -0,0 +1,12 @@
+/* globals describe it expect */
+const Ship = require('../src/Ship.js');
+
+describe('Ship', () => {
+    it('can be instantiated', () => {
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
+    it('has a starting port', () => {
+        const ship = new Ship('Dover');
+        expect(ship.startingPort).toBe('Dover');
+    });
+});
