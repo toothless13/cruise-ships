967599064ace4c3c73b314b3ce872cab76546e4e
Tim <50870607+toothless13@users.noreply.github.com>
2023-07-18 15:01:57 +0100

    make Port.test.js DRY

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -3,50 +3,48 @@ const Port = require('../src/Port');
 const Ship = require('../src/Ship');
 
 describe('Port', () => {
+
+    describe('tests with ports, itinerary and ship', () => {
+
+        let port1, port2, itinerary, ship1;
+
+        beforeEach(() => {
+            port1 = new Port('Dover');
+            port2 = new Port('Belfast');
+            itinerary = new Itinerary([port1, port2]);
+            ship1 = new Ship(itinerary);
+        });
+
+        it('has a name property', () => {
+            expect(port1).toHaveProperty('name');
+            expect(port1.name).toBe('Dover');
+            expect(port2.name).toBe('Belfast');
+        });
+    
+        describe('addShip', () => {
+            it('adds a ship to a docked ships array property', () => {
+                port1.addShip(ship1);
+                expect(port1.ships).toContain(ship1);
+            });
+        });
+        
+        describe('removeShip', () => {
+            it('removes a ship from the docked ships array', () => {
+                port1.removeShip(ship1);
+                expect(port1.ships).toEqual([]);
+            });
+        
+            it('removes a ship from docked ships array when multiple ships are in array', () => {
+                const ship2 = new Ship(itinerary);
+                const ship3 = new Ship(itinerary);
+        
+                port1.removeShip(ship2);
+                expect(port1.ships).not.toContain(ship2);
+            });
+        });
+    });
     it('can be instantiated', () => {
         expect(new Port()).toBeInstanceOf(Object);
     });
-    it('has a name property', () => {
-        const port = new Port();
-        expect(port).toHaveProperty('name');
-        const port2 = new Port('Dover');
-        expect(port2.name).toBe('Dover');
-    });
-});
-
-describe('addShip', () => {
-    it('adds a ship to a docked ships array property', () => {
-        const port1 = new Port('Dover');
-        const port2 = new Port('Belfast');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship1 = new Ship(itinerary);
-
-        port1.addShip(ship1);
-        expect(port1.ships).toContain(ship1);
-    });
-});
-
-describe('removeShip', () => {
-    it('removes a ship from the docked ships array', () => {
-        const port1 = new Port('Dover');
-        const port2 = new Port('Belfast');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship1 = new Ship(itinerary);
-
-        // port1.addShip(ship1);
-        port1.removeShip(ship1);
-        expect(port1.ships).toEqual([]);
-    });
-
-    it('removes a ship from docked ships array when multiple ships are in array', () => {
-        const port1 = new Port('Dover');
-        const port2 = new Port('Belfast');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship1 = new Ship(itinerary);
-        const ship2 = new Ship(itinerary);
-        const ship3 = new Ship(itinerary);
-        
-        port1.removeShip(ship2);
-        expect(port1.ships).not.toContain(ship2);
-    });
+    
 });
