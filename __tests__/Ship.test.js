9c425713652d39f21536fccd79c322f7b1876b6e
Tim <50870607+toothless13@users.noreply.github.com>
2023-07-18 14:46:05 +0100

    make Ship.test.js DRY

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -4,70 +4,57 @@ const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary');
 
 describe('Ship', () => {
+
     it('can be instantiated', () => {
         const port = new Port('Dover');
         const itinerary = new Itinerary([port]);
         expect(new Ship(itinerary)).toBeInstanceOf(Object);
     });
 
-    it('has a starting port', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
-        expect(ship.currentPort).toBeInstanceOf(Object);
-        const ship2 = new Ship(itinerary);
-        expect(ship2.currentPort).toBe(port);
-    });
-
-    it('has a previousPort property set to null', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
-        expect(ship).toHaveProperty('previousPort');
-        expect(ship.previousPort).toBeNull();
-    });
-
-    it('can set sail', () => {
-        const dover = new Port('Dover');
-        const belfast = new Port('Belfast');
-        const itinerary = new Itinerary([dover, belfast]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        expect(ship.currentPort).toBeFalsy;
-        expect(ship.previousPort).toBe(dover);
-        expect(dover.ships).not.toContain(ship);
-    });
-
-    it('can\'t sail further than its itinerary', () => {
-        const dover = new Port('Dover');
-        const belfast = new Port('Belfast');
-        const itinerary = new Itinerary([dover, belfast]);
-        const ship = new Ship(itinerary);
-
-        ship.setSail();
-        ship.dock();
-
-        expect(() => ship.setSail()).toThrowError('End of itinerary reached');
-    });
-
-    it('can dock', () => {
-        const dover = new Port('Dover');
-        const belfast = new Port('Belfast');
-        const itinerary = new Itinerary([dover, belfast]);
-        const ship = new Ship(itinerary);
+    describe('tests with one port', () => {
         
-        ship.setSail();
-        ship.dock();
-        expect(ship.currentPort).toBe(belfast);
-        expect(belfast.ships).toContain(ship);
-    });
+        let dover, belfast, itinerary, ship;
 
-    it('gets added to port on instantiation', () => {
-        const dover = new Port('Dover');
-        // const belfast = new Port('Belfast');
-        const itinerary = new Itinerary([dover]);
-        const ship = new Ship(itinerary);
+        beforeEach(() => {
+            dover = new Port('Dover');
+            belfast = new Port('Belfast');
+            itinerary = new Itinerary([dover, belfast]);
+            ship = new Ship(itinerary);
+        });
+        it('has a starting port', () => {
+            expect(ship.currentPort).toBeInstanceOf(Object);
+            const ship2 = new Ship(itinerary);
+            expect(ship2.currentPort).toBe(dover);
+        });
+    
+        it('has a previousPort property set to null', () => {
+            expect(ship).toHaveProperty('previousPort');
+            expect(ship.previousPort).toBeNull();
+        });
 
-        expect(dover.ships).toContain(ship);
+        it('gets added to port on instantiation', () => {    
+            expect(dover.ships).toContain(ship);
+        });
+
+        it('can set sail', () => {
+            ship.setSail();
+            expect(ship.currentPort).toBeFalsy;
+            expect(ship.previousPort).toBe(dover);
+            expect(dover.ships).not.toContain(ship);
+        });
+    
+        it('can\'t sail further than its itinerary', () => {  
+            ship.setSail();
+            ship.dock();
+    
+            expect(() => ship.setSail()).toThrowError('End of itinerary reached');
+        });
+    
+        it('can dock', () => {
+            ship.setSail();
+            ship.dock();
+            expect(ship.currentPort).toBe(belfast);
+            expect(belfast.ships).toContain(ship);
+        });
     });
 });
