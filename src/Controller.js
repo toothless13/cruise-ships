8966fe90ace2f6ce4931ad2f2d768aefbc119166
Tim <50870607+toothless13@users.noreply.github.com>
2023-07-25 14:29:31 +0100

    add renderPorts method to Controller

--- src/Controller.js
+++ src/Controller.js
@@ -3,6 +3,7 @@
         constructor() {
             this.initialiseSea();
         }
+
         initialiseSea() {
             const backgrounds = [
                 '../images/water0.png',
@@ -13,6 +14,21 @@
                 document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
                 backgroundIndex += 1;
             }, 1000);
+        }
+
+        renderPorts(ports) {
+            const portsElement = document.querySelector('#ports');
+            let portsElementWidth = 0;
+            portsElement.style.width = portsElementWidth + 'px';
+            ports.forEach((port) => {
+                console.log(port);
+                const div = document.createElement('div');
+                div.setAttribute('class', 'port');
+                div.setAttribute('data-portIndex', ports.indexOf(port));
+                portsElementWidth += 256;
+                portsElement.style.width = portsElementWidth + 'px';
+                portsElement.appendChild(div);
+            });
         }
     }
 
